3fd6ad848c945593dcc8f9e514173b12df2dfe1f
ngjamesng <just.james.ng@gmail.com>
2020-02-05 17:15:04 -0800

    add test for followers, signup. Will need to fix test for username already existing

--- test_user_model.py
+++ test_user_model.py
@@ -7,6 +7,7 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
 
 from models import db, User, Message, Follows
 
@@ -41,6 +42,15 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+        user = User(
+            email="OURTEST@test.com",
+            username="TESTUSERNAME",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(user)
+        db.session.commit() 
+
     def test_user_model(self):
         """Does basic model work?"""
 
@@ -56,3 +66,65 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_repr(self):
+        """test the repr"""
+
+        user = User.query.filter_by(username="TESTUSERNAME").first()
+
+        self.assertEqual(str(user), f"<User #{user.id}: {user.username}, {user.email}>")
+        
+    def test_is_following(self):
+        """test if user1 is following user2"""
+
+        user1 = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        user2 = User.query.filter_by(username="TESTUSERNAME").first()
+
+        user1.following.append(user2)
+        db.session.add(user1)
+        db.session.commit()
+
+        self.assertEqual(user1.is_following(user2), True) 
+        self.assertEqual(user2.is_following(user1), False)
+
+    def test_is_followed_by(self):
+        """test if user1 is following user2"""
+
+        user1 = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        user2 = User.query.filter_by(username="TESTUSERNAME").first()
+
+        user1.following.append(user2)
+        db.session.add(user1)
+        db.session.commit()
+        
+        self.assertEqual(user1.is_followed_by(user2), False) 
+        self.assertEqual(user2.is_followed_by(user1), True)
+
+    def test_signup(self):
+
+        with self.client as client:
+
+            user = User.signup("user1", "user1@email.com", "password1", None)
+            db.session.commit()
+            # user2 = User.signup("user1", "user1@email.com", "password1", None)
+            # db.session.commit()
+
+            # self.assertIsInstance(user, User)
+            # with self.assertRaises(IntegrityError):
+            #     User.signup("user1", "user1@email.com", "password1", None)
+            #     db.session.commit()
+
+            self.assertIsInstance(user, User)
+            with self.assertRaises(InvalidRequestError):
+                User.signup("user1", "user1@email.com", "password1", None)
+                db.session.commit()
